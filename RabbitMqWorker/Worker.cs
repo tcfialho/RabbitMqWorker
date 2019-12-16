186fc2a8ad116d611a89e6a4b49191c72b7ad932
Thiago de Castro Fialho <tcfialho@gmail.com>
2019-12-15 23:03:12 -0300

    Refactoring

--- RabbitMqWorker/Worker.cs
+++ RabbitMqWorker/Worker.cs
@@ -15,6 +15,7 @@ namespace RabbitMqWorker
         private readonly IModel _channel;
         private readonly ILogger<Worker> _logger;
         private readonly RabbitMQConfig _rabbitMQConfig;
+        private readonly AsyncEventingBasicConsumer _consumer;
 
         public Worker(IModel channel,
                       ILogger<Worker> logger,
@@ -23,25 +24,22 @@ namespace RabbitMqWorker
             _channel = channel;
             _logger = logger;
             _rabbitMQConfig = rabbitMQConfig;
+            _consumer = new AsyncEventingBasicConsumer(_channel);
+            _channel.BasicConsume(_rabbitMQConfig.QueueName, false, _consumer);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             stoppingToken.ThrowIfCancellationRequested();
 
-            var consumer = new AsyncEventingBasicConsumer(_channel);
-
-            consumer.Received += Consumer_Received;
-
-            _channel.BasicConsume(_rabbitMQConfig.QueueName, false, consumer);
+            _consumer.Received += async (sender, args) =>
+            {
+                _logger.LogInformation("Received {@content} as consumer {@consumer}", Encoding.UTF8.GetString(args.Body), args.ConsumerTag);
+                _channel.BasicAck(args.DeliveryTag, false);
+                await Task.CompletedTask;
+            };
 
             await Task.CompletedTask;
-        }
-
-        private async Task Consumer_Received(object sender, BasicDeliverEventArgs args)
-        {
-            _logger.LogInformation("Received {@content} as consumer {@consumer}", Encoding.UTF8.GetString(args.Body), args.ConsumerTag);
-            _channel.BasicAck(args.DeliveryTag, false);
         }
     }
 }
